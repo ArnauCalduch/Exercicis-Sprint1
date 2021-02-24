d96115d61853ee8988ea2f2b069f4869d6426731
Arnau Calduch <65604168+ArnauCalduch@users.noreply.github.com>
2021-02-24 11:38:32 +0100

    Add files via upload

---
+++ Functions&TemplatesLiterals/ex1.js
@@ -0,0 +1,8 @@
+//Variable del nom
+const name = 'Miquel';
+//Funció que imprimeix a la consola el nom passat com a paràmetre
+function printName(name){
+    console.log(name);
+}
+
+printName(name);
